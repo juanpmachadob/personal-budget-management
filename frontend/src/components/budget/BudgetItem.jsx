d24eaf7ea4d87fc78db5494809ee01830dcde0bd
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-05-24 16:45:11 -0500

    feat(frontend): add edit and delete icons with styles

--- frontend/src/components/budget/BudgetItem.jsx
+++ frontend/src/components/budget/BudgetItem.jsx
@@ -1,11 +1,27 @@
+import BiEditAlt from "../ui/BiEditAlt";
+import BiTrashAlt from "../ui/BiTrashAlt";
+
 const BudgetItem = () => {
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
   return (
     <tr className="table__row">
       <td className="table__cell">Lorem, ipsum.</td>
       <td className="table__cell table__cell--income">+ $5000</td>
       <td className="table__cell">10/120/12 23:2pm</td>
       <td className="table__cell">Lorem, ipsum.</td>
-      <td className="table__cell">Edit, delete</td>
+      <td className="table__cell">
+        <BiEditAlt
+          className="table__icon table__icon--blue"
+          onClick={handleEdit}
+        />
+        <BiTrashAlt
+          className="table__icon table__icon--red"
+          onClick={handleDelete}
+        />
+      </td>
     </tr>
   );
 };

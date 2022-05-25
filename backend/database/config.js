399a5c69054aca02dd8c355d045d9483f5018b8f
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-05-24 19:44:36 -0500

    build(backend): Installation and basic configuration

---
+++ backend/database/config.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    define: {
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    // logging: false
+  }
+);
+
+module.exports = db;
---
+++ backend/models/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const db = require("../database/config");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 5000;
+
+    this.paths = {
+      auth: "/api/auth",
+      budget: "/api/budget",
+      search: "/api/search",
+    };
+
+    this.connectDB();
+    this.middlewares();
+    this.routes();
+  }
+
+  connectDB() {
+    db.authenticate()
+      .then(() =>
+        console.log("MySQL DB connection established successfully!")
+      )
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  middlewares() {
+    this.app.use(cors());
+
+    this.app.use(express.json());
+
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/public/index.html"));
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
